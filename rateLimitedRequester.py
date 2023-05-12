562e758103472ac27287ca17cefac438a224044a
mago3421 <marc.gonzalez@colorado.edu>
2023-05-11 20:01:27 -0600

    Add files via upload
    Scraping code works but no predictor

--- rateLimitedRequester.py
+++ rateLimitedRequester.py
@@ -13,22 +13,33 @@ class rateLimitedRequester:
         self.batchEnd = 0
         # Max allowable requests per minute
         self.rpm = rpm
-        
+
+    def rlrequest(self, URL):
+        try:
+            data = urllib.request.urlopen(URL).read()
+        except Exception:
+            return None
+        time.sleep(3)
+        return data
+
+"""     
     # Limits requests to rpm
     def rlrequest(self, URL):
-        # Make request
-        data = urllib.request.urlopen(URL).read()
-        # Restart timer if requests is at zero
-        if self.batchRequests == 0:
-            self.batchEnd = time.time() + 60 # 60 seconds in future
-        # Increment requests
-        self.batchRequests += 1
         # Check if too many requests per minute have been made
         if self.batchRequests >= self.rpm:
             # Check if there is time remaining
             timeRemaining = self.batchEnd - time.time()
-            if timeRemaining > 0:
-                # Sleep for the minute before returning data
+            if timeRemaining > 0.0:
+                # Sleep for the minute before making request
                 time.sleep(timeRemaining)
             self.batchRequests = 0
+        # Make request
+        data = urllib.request.urlopen(URL).read()
+        # Restart timer if requests is at zero
+        if self.batchRequests == 0:
+            self.batchEnd = time.time() + 60.0
+        # Increment requests
+        self.batchRequests += 1         
         return data
+"""
+
--- webDataExtract.py
+++ webDataExtract.py
@@ -5,6 +5,7 @@ Notes: Requires Python 3.6+
 """
 
 import os
+import traceback
 from bs4 import BeautifulSoup
 from rateLimitedRequester import rateLimitedRequester
 
@@ -15,15 +16,15 @@ PARENT_URL="https://www.sports-reference.com"
 # Season starts and end per year
 SEASONS = { \
     "2022-2023": \
-        {"start": {"y": 2022, "m": 11, "d": 9}, \
+        {"start": {"y": 2022, "m": 12, "d": 1}, \
          "end": {"y": 2023, "m": 4, "d": 3}}, \
-    "2022-2023": \
-        {"start": {"y": 2022, "m": 11, "d": 7}, \
-         "end": {"y": 2023, "m": 4, "d": 4}}, \
-}
+    "2021-2022": \
+        {"start": {"y": 2021, "m": 11, "d": 8}, \
+         "end": {"y": 2022, "m": 4, "d": 4}}, \
+} # Originally d=7
 
 # Days per month dictionary (NOTE: Python 3.6+ will maintain order by default for advancing month)
-MONTHS = {1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
+MONTHS = {1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 30} # 12=31
 
 # Top-level directory for data (Change as needed)
 DATADIR = os.path.join(os.path.expanduser('~'), "ncaamb-or-net", "gamedata")
@@ -35,6 +36,47 @@ def changeURLDate(url, m, d, y):
     url = url.replace("YYYY", "{:04d}".format(y))
     return url
 
+def processTeamData(gameDir, pageTables, team_Name, teamId, index):
+    try:
+        # Team 1 player stat Processing
+        teamStats = pageTables[index].find("tbody").find_all("tr")
+        # Open team 1 stat file
+        teamFile = open("{}/{}.csv".format(gameDir, team_Name),"w+")
+        # Get team 1 player stats
+        for playerTr in teamStats:
+            playerStats = []
+            # Check for Reserves column
+            if "Reserves" in playerTr.find(attrs={"data-stat":"player"}).text: continue
+            playerStats.append(playerTr.find(attrs={"data-stat":"player"}).text) # Name
+            playerStats.append(playerTr.find(attrs={"data-stat":"mp"}).text) # Minutes played
+            playerStats.append(playerTr.find(attrs={"data-stat":"fg"}).text) # Field goals
+            playerStats.append(playerTr.find(attrs={"data-stat":"fga"}).text) # Field goals attempted
+            playerStats.append(playerTr.find(attrs={"data-stat":"fg2"}).text) # 2pt field goals
+            playerStats.append(playerTr.find(attrs={"data-stat":"fg2a"}).text) # 2pt field goals
+            playerStats.append(playerTr.find(attrs={"data-stat":"fg3"}).text) # 3pt field goals
+            playerStats.append(playerTr.find(attrs={"data-stat":"fg3a"}).text) # 3pt field goals attempted
+            playerStats.append(playerTr.find(attrs={"data-stat":"ft"}).text) # Free throws
+            playerStats.append(playerTr.find(attrs={"data-stat":"fta"}).text) # Free throws attempted
+            playerStats.append(playerTr.find(attrs={"data-stat":"orb"}).text) # Offensive rebounds
+            playerStats.append(playerTr.find(attrs={"data-stat":"drb"}).text) # Defensive rebounds
+            playerStats.append(playerTr.find(attrs={"data-stat":"trb"}).text) # Total rebounds
+            playerStats.append(playerTr.find(attrs={"data-stat":"ast"}).text) # Assists
+            playerStats.append(playerTr.find(attrs={"data-stat":"stl"}).text) # Steals
+            playerStats.append(playerTr.find(attrs={"data-stat":"tov"}).text) # Turnovers
+            playerStats.append(playerTr.find(attrs={"data-stat":"pf"}).text) # Personal fouls
+            playerStats.append(playerTr.find(attrs={"data-stat":"pts"}).text) # Points
+            # Write stats to file
+            teamFile.write(','.join(playerStats))
+            teamFile.write('\n')
+        teamFile.close()
+    # If there is an exception while processing the team information, print to log
+    except Exception:
+        print("Error processing team: {}\n".format(team_Name))
+        tlog = open("{}/traceback{}.txt".format(gameDir, teamId), "w+")
+        tlog.write('teamName={}\n'.format(team_Name))
+        traceback.print_exc(file=tlog)
+        tlog.close()
+
 def recordGame(page, y, m, d):
     data = BeautifulSoup(page, 'html.parser')  
     # Line Score
@@ -45,23 +87,36 @@ def recordGame(page, y, m, d):
     ls_table = BeautifulSoup(temp.replace("<!--","").replace("-->",""), 'html.parser')
     trs = ls_table.find_all("tr")
     # Team 1 data
-    team1_Name = trs[2].find(attrs={"data-stat":"team"}).text # Name
+    team1_Name = trs[2].find(attrs={"data-stat":"team"}).text
+    # Check if Team 1 has a link to player data
+    team1_Link = trs[2].find(attrs={"data-stat":"team"}).find("a")
     # Replace spaces in team name with underscores
     team1_Name = team1_Name.replace(" ","-")
+    # Strip other special characters
+    team1_Name = team1_Name.replace("&","").replace("'","").replace("(","").replace(")","").replace(".","")
+    # Get team 1 Scores
     team1_1HS = trs[2].find(attrs={"data-stat":"1"}).text # 1st Half Score
     team1_2HS = trs[2].find(attrs={"data-stat":"2"}).text # 2nd Half Score
     team1_TS = trs[2].find(attrs={"data-stat":"T"}).text # Total Score
     # Team 2 data
-    team2_Name = trs[3].find(attrs={"data-stat":"team"}).text # Name
+    team2_Name = trs[3].find(attrs={"data-stat":"team"}).text
+    team2_Link = trs[3].find(attrs={"data-stat":"team"}).find("a")
     # Replace spaces in team name with underscores
-    team2_Name = team2_Name.replace(" ","-")        
+    team2_Name = team2_Name.replace(" ","-")
+    # Strip other special characters
+    team2_Name = team2_Name.replace("&","").replace("'","").replace("(","").replace(")","").replace(".","")
+    # Get team 2 Scores      
     team2_1HS = trs[3].find(attrs={"data-stat":"1"}).text # 1st Half Score
     team2_2HS = trs[3].find(attrs={"data-stat":"2"}).text # 2nd Half Score
     team2_TS = trs[3].find(attrs={"data-stat":"T"}).text # Total Score
     # Make directory for game
-    gameDir = "{0}/{1}-{2}-{3}_{4}-{5}".format(DATADIR, "{:04d}".format(y),"{:02d}".format(m),"{:02d}".format(d), team1_Name, team2_Name)
+    gameDir = "{0}/{1}-{2}-{3}_{4}_{5}".format(DATADIR, "{:04d}".format(y),"{:02d}".format(m),"{:02d}".format(d), team1_Name, team2_Name)
     # Use relative path
-    os.mkdir(gameDir)
+    try:
+        os.mkdir(gameDir)
+    # Some games appear twice
+    except FileExistsError:
+        return
     # Write to file
     outcome = open("{}/outcome.csv".format(gameDir),"w+")
     outcome.write(str(",".join([team1_Name,team1_1HS,team1_2HS,team1_TS])))
@@ -70,69 +125,13 @@ def recordGame(page, y, m, d):
     outcome.close()
     # End line score processing
     # Get the rest of statistic tables
-    pageTables = data.find_all("table")    
-    # Team 1 player stat Processing
-    team1Stats = pageTables[1].find("tbody").find_all("tr")
-    # Open team 1 stat file
-    team1File = open("{}/{}.csv".format(gameDir, team1_Name),"w+")
-    # Get team 1 player stats
-    for playerTr in team1Stats:
-        playerStats = []
-        # Check for Reserves column
-        if "Reserves" in playerTr.find(attrs={"data-stat":"player"}).text: continue
-        playerStats.append(playerTr.find(attrs={"data-stat":"player"}).text) # Name
-        playerStats.append(playerTr.find(attrs={"data-stat":"mp"}).text) # Minutes played
-        playerStats.append(playerTr.find(attrs={"data-stat":"fg"}).text) # Field goals
-        playerStats.append(playerTr.find(attrs={"data-stat":"fga"}).text) # Field goals attempted
-        playerStats.append(playerTr.find(attrs={"data-stat":"fg2"}).text) # 2pt field goals
-        playerStats.append(playerTr.find(attrs={"data-stat":"fg2a"}).text) # 2pt field goals
-        playerStats.append(playerTr.find(attrs={"data-stat":"fg3"}).text) # 3pt field goals
-        playerStats.append(playerTr.find(attrs={"data-stat":"fg3a"}).text) # 3pt field goals attempted
-        playerStats.append(playerTr.find(attrs={"data-stat":"ft"}).text) # Free throws
-        playerStats.append(playerTr.find(attrs={"data-stat":"fta"}).text) # Free throws attempted
-        playerStats.append(playerTr.find(attrs={"data-stat":"orb"}).text) # Offensive rebounds
-        playerStats.append(playerTr.find(attrs={"data-stat":"drb"}).text) # Defensive rebounds
-        playerStats.append(playerTr.find(attrs={"data-stat":"trb"}).text) # Total rebounds
-        playerStats.append(playerTr.find(attrs={"data-stat":"ast"}).text) # Assists
-        playerStats.append(playerTr.find(attrs={"data-stat":"stl"}).text) # Steals
-        playerStats.append(playerTr.find(attrs={"data-stat":"tov"}).text) # Turnovers
-        playerStats.append(playerTr.find(attrs={"data-stat":"pf"}).text) # Personal fouls
-        playerStats.append(playerTr.find(attrs={"data-stat":"pts"}).text) # Points
-        # Write stats to file
-        team1File.write(','.join(playerStats))
-        team1File.write('\n')
-    team1File.close()
-    # Team 2 player stat Processing
-    team2Stats = pageTables[3].find("tbody").find_all("tr")
-    # Open team 1 stat file
-    team2File = open("{}/{}.csv".format(gameDir, team2_Name),"w+")
-    # Get team 2 player stats
-    for playerTr in team2Stats:
-        playerStats = []        
-        # Check for Reserves column and skip
-        if "Reserves" in playerTr.find(attrs={"data-stat":"player"}).text: continue
-        playerStats.append(playerTr.find(attrs={"data-stat":"player"}).text) # Name
-        playerStats.append(playerTr.find(attrs={"data-stat":"mp"}).text) # Minutes played
-        playerStats.append(playerTr.find(attrs={"data-stat":"fg"}).text) # Field goals
-        playerStats.append(playerTr.find(attrs={"data-stat":"fga"}).text) # Field goals attempted
-        playerStats.append(playerTr.find(attrs={"data-stat":"fg2"}).text) # 2pt field goals
-        playerStats.append(playerTr.find(attrs={"data-stat":"fg2a"}).text) # 2pt field goals
-        playerStats.append(playerTr.find(attrs={"data-stat":"fg3"}).text) # 3pt field goals
-        playerStats.append(playerTr.find(attrs={"data-stat":"fg3a"}).text) # 3pt field goals attempted
-        playerStats.append(playerTr.find(attrs={"data-stat":"ft"}).text) # Free throws
-        playerStats.append(playerTr.find(attrs={"data-stat":"fta"}).text) # Free throws attempted
-        playerStats.append(playerTr.find(attrs={"data-stat":"orb"}).text) # Offensive rebounds
-        playerStats.append(playerTr.find(attrs={"data-stat":"drb"}).text) # Defensive rebounds
-        playerStats.append(playerTr.find(attrs={"data-stat":"trb"}).text) # Total rebounds
-        playerStats.append(playerTr.find(attrs={"data-stat":"ast"}).text) # Assists
-        playerStats.append(playerTr.find(attrs={"data-stat":"stl"}).text) # Steals
-        playerStats.append(playerTr.find(attrs={"data-stat":"tov"}).text) # Turnovers
-        playerStats.append(playerTr.find(attrs={"data-stat":"pf"}).text) # Personal fouls
-        playerStats.append(playerTr.find(attrs={"data-stat":"pts"}).text) # Points
-        # Write stats to file
-        team2File.write(','.join(playerStats))
-        team2File.write('\n')
-    team2File.close() 
+    pageTables = data.find(id="boxes").find_all("table")
+    # Process team 1 if available
+    if team1_Link is not None:
+        processTeamData(gameDir, pageTables, team1_Name, 1, 0)
+    # Process team 2 if available
+    if team2_Link is not None:
+        processTeamData(gameDir, pageTables, team2_Name, 2, (2 if team1_Link is not None else 0))
 
 def extractSeason(start, end):
     # Use rate-limiting class to limit requests to page at 20 per minute
@@ -157,12 +156,15 @@ def extractSeason(start, end):
             # Iterate through game divs
             for gameDiv in gameDivs:
                 # For each men's game, get the link to the game
-                gameLinkTd = gameDiv.find("td", {"class":"right gamelink"})
-                # Append parent URL to link
-                gameURL = PARENT_URL + gameLinkTd.find('a')["href"] 
-                # Now process the game
-                gamePage = rlr.rlrequest(gameURL)
-                recordGame(gamePage, y, m, d)
+                gameLink = gameDiv.find("td", {"class":"right gamelink"}).find('a')
+                # Some games have no record
+                if gameLink is not None:
+                    # Append parent URL to link
+                    gameURL = PARENT_URL + gameLink["href"]
+                    # Now process the game
+                    gamePage = rlr.rlrequest(gameURL)
+                    if gamePage is not None:
+                        recordGame(gamePage, y, m, d)
             # Check for end of season and exit loop if true
             if (y == end["y"]) and (m == end["m"]) and (d == end["d"]):
                 seasonOver = True
@@ -172,6 +174,8 @@ def extractSeason(start, end):
                 d += 1
         # Move onto next month
         m += 1
+        # Reset days
+        d = 1
         # Advance year if necessary
         if m == 13:
             m = 1
@@ -183,9 +187,15 @@ if __name__=="__main__":
     originalDir = DATADIR
     # Now change DATADIR to season-specific
     DATADIR = os.path.join(originalDir,"2022-2023")
-    os.mkdir(DATADIR)
-    extractSeason(start=SEASONS["2022-2023"]["start"], end={"y":"2022","m":"3","d":"1"})
+    try:
+        os.mkdir(DATADIR)
+    except FileExistsError:
+        pass
+    extractSeason(start=SEASONS["2022-2023"]["start"], end={"y":"2022","m":"3","d":"12"})
     # Now change DATADIR to season-specific
     DATADIR = os.path.join(originalDir,"2021-2022")
-    os.mkdir(DATADIR)
-    extractSeason(start=SEASONS["2021-2022"]["start"], end=SEASONS["2021-2022"]["	end"])
+    try:
+        os.mkdir(DATADIR)
+    except FileExistsError:
+        pass
+    extractSeason(start=SEASONS["2021-2022"]["start"], end=SEASONS["2021-2022"]["end"])
